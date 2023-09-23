d729a86984b036ef096fa1a248eff557f3337c81
Fred Dixon <ffdixon@gmail.com>
2023-09-23 18:58:19 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -ex
 
 
-rm *.deb
+rm -f *.deb
 fpm -s dir -t deb -n hello-world -v 1.0.0 --prefix=/usr/local/bin/ --chdir=./target/release/ hello-world
 dpkg -c hello-world_1.0.0_amd64.deb
 
