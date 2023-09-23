2013f618c292656862b36755a33665038d7f5191
Fred Dixon <ffdixon@gmail.com>
2023-09-23 23:15:52 +0000

    Test #9

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 #!/bin/bash -ex
 
 
+pushd hello-world
+cargo build --release
+popd
+
 rm -f *.deb
 fpm -s dir -t deb -n hello-world -v 1.0.0 --prefix=/usr/local/bin/ --chdir=./hello-world/target/release/ hello-world
 dpkg -c hello-world_1.0.0_amd64.deb
