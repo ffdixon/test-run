3ceeb5b07574b6ea46778d990d8fae827547c936
Fred Dixon <ffdixon@gmail.com>
2023-09-23 22:59:50 +0000

    Test #5

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 
 
 rm -f *.deb
-fpm -s dir -t deb -n hello-world -v 1.0.0 --prefix=/usr/local/bin/ --chdir=./target/release/ hello-world
+fpm -s dir -t deb -n hello-world -v 1.0.0 --prefix=/usr/local/bin/ --chdir=./hello-world/target/release/ hello-world
 dpkg -c hello-world_1.0.0_amd64.deb
 
